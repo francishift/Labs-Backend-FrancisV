9f9d6524718b44fd4a33438d4e5653353128542c
Francis Valenzuela <francis@valenzuela.com>
2026-04-28 12:52:48 +0000

    chore: remove temporary test scripts from root

--- test_recur.php
+++
@@ -1,6 +0,0 @@
-<?php
-$id = "_8crjeh1i6oqjcb9k8kp3eb9k6sq46ba2851jiba365136h9m6grk2e1o8o_20260801T070000Z";
-$parts = explode('_', $id);
-$datePart = array_pop($parts);
-$masterId = implode('_', $parts);
-echo "Master: " . $masterId . "\nDate: " . $datePart . "\n";
--- test_regex.php
+++
@@ -1,5 +0,0 @@
-<?php
-$str = "_8crjeh1i6oqjcb9k8kp3eb9k6sq46ba2851jiba365136h9m6grk2e1o8o_20260901T070000Z";
-$regex = '/_([0-9]{8})(T[0-9]{6}Z)?$/';
-var_dump(preg_match($regex, $str));
-var_dump(preg_replace($regex, '', $str));
